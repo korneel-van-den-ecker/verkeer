f492292c764ea476a180006eb9fac5c3d3c1bafe
korneel-van-den-ecker <korneel_van_den_ecker@hotmail.com>
2020-08-03 15:06:01 +0200

    met Google Maps --> Te duur

--- src/components/features/ScriptService/Signalisatie.js
+++ src/components/features/ScriptService/Signalisatie.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import {useDispatch} from 'react-redux'
 import { useSelector } from "react-redux";
+import GoogleMap from '../Map/GoogleMap'
 
 import {
   selectSignalisatie,
@@ -23,6 +24,7 @@ const ScriptService = ({ scriptService, ...props }) => {
 
   return (
     <div>
+      <GoogleMap></GoogleMap>
       {signalisatiesloading? <CircularProgress></CircularProgress>:
       signalisaties.rssverkeersdata.rss_bord.map(item=><p>{item.abbameldanaam}</p>)}
     </div>
